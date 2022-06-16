4b0425a74b881368a8782f223b18edf70cf85208
Ivo - IXP <ivo@ixp.nl>
2022-06-16 02:40:24 +0200

    more gameplay

--- public/game.js
+++ public/game.js
@@ -80,10 +80,10 @@ const positions = {
     bf2: { x: 600, y: 900 },
     bf3: { x: 600, y: 800 },
     bf4: { x: 600, y: 700 },
-    yf2: { x: 300, y: 600 },
     yf1: { x: 200, y: 600 },
-    yf3: { x: 500, y: 600 },
-    yf4: { x: 400, y: 600 }
+    yf2: { x: 300, y: 600 },
+    yf3: { x: 400, y: 600 },
+    yf4: { x: 500, y: 600 }
 };
 
 const start = {
@@ -112,6 +112,13 @@ const pawns = {
     y4: { p: 'yh4', s: 0 },
 }
 
+const players = [
+    { color: 'b', name: '' },
+    { color: 'y', name: '' },
+    { color: 'g', name: '' },
+    { color: 'r', name: '' }
+];
+
 const boardSvg = document.getElementById('board');
 
 let dice = 0;
@@ -137,8 +144,9 @@ function createBoard() {
         circle.setAttribute('stroke-width', '5');
         circle.setAttribute('cx', pos.x)
         circle.setAttribute('cy', pos.y)
-        circle.setAttribute('r', key[1] == 'f' ? '30': (key[1] == 'h' ? '34' : '38'))
+        circle.setAttribute('r', key[1] === 'f' ? '30': (key[1] === 'h' ? '34' : '38'))
         boardSvg.appendChild(circle);
+        pos.o = circle;
     }
 
     // add text
@@ -177,10 +185,10 @@ function createBoard() {
         boardSvg.appendChild(group);
     });
 
-    addName(150,275, 'name1');
-    addName(1050,275, 'name2');
-    addName(150,1175, 'name0');
-    addName(1050,1175, 'name3');
+    addName(150,275, 'name-y');
+    addName(1050,275, 'name-g');
+    addName(150,1175, 'name-b');
+    addName(1050,1175, 'name-r');
 }
 
 function addText(x, y, text) {
@@ -226,16 +234,34 @@ function addPawns() {
         boardSvg.appendChild(group);
         pawn.o = group;
         positions[pawn.p].p = key;
+
+        // pawn click handler
         group.addEventListener('click', () => {
-            clickPawn(key);
+            if ((pawn.o.dataset.movable === 'y') && pawn.n) {
+                resetMovable();
+                movePawn(pawn);
+            }
         });
+
+        // highlight gerelateerde positie bij mouse-over
+        group.addEventListener('mouseenter', () => {
+            if ((pawn.o.dataset.movable === 'y') && pawn.n) {
+                positions[pawn.n].o.classList.add('highlight');
+            }
+        });
+        group.addEventListener('mouseleave', () => {
+            if (pawn.n) {
+                positions[pawn.n].o.classList.remove('highlight');
+            }
+        });
+
     }
 }
 
-function movePawn(key, steps) {
+function XmovePawn(key, steps) {
     const pawn = pawns[key];
     const color = key[0];
-    if (pawn == undefined) {
+    if (pawn === undefined) {
         return false;
     }
     let newpos = null;
@@ -250,7 +276,7 @@ function movePawn(key, steps) {
     // remove pawn from board
     else if (steps === -1) {
         for (let i = 1; i <= 4; i++) {
-             if (positions[color + 'h' + String(i)].p == undefined) {
+             if (positions[color + 'h' + String(i)].p === undefined) {
                  newpos = color + 'h' + String(i);
                  pawn.s = 0;
                  break;
@@ -295,16 +321,14 @@ function movePawn(key, steps) {
 
 }
 
-function setActive(color) {
-
-}
-
-const players = ['b', 'y', 'g', 'r' ];
 let player = -1;
 
-let playernames = [];
-
-let gamestate;
+let game = {
+    dice: 0,
+    state: '',
+    activePlayer: 0,
+    throwCount: 0
+};
 
 // document.getElementById('movebutton').addEventListener('click', (e) => {
 //     doStep();
@@ -315,16 +339,18 @@ let gamestate;
 //     play();
 // });
 
-function play() {
+/*
+function autoplay() {
     doStep();
     setTimeout(() => {
-        play();
+        autoplay();
     }, 100);
 }
+*/
 
 function doStep() {
-    player = player == 3 ? 0 : player + 1;
-    const color = players[player];
+    player = player === 3 ? 0 : player + 1;
+    const color = players[player].color;
     let steps = 1 + Math.floor(Math.random() * 6);
     let pawn;
     if (steps === 6) {
@@ -347,7 +373,7 @@ function doStep() {
             }
         }
     }
-    if (pawn != undefined) {
+    if (pawn !== undefined) {
         console.log(color.toUpperCase() + ' throws: ' + steps + ' and move pawn ' + pawn.toUpperCase() + ' forward');
         movePawn(pawn, steps);
         return;
@@ -360,25 +386,28 @@ document.getElementById('dicebtn').addEventListener('click', (e) => {
     rollDice();
 });
 
-document.getElementById('dice').addEventListener('thrown', (e) => {
-    console.log(e.value);
-    document.getElementById('dicebtn').removeAttribute('disabled');
-    // movePawn('y1', e.value);
-});
+// document.getElementById('dice').addEventListener('thrown', (e) => {
+//     console.log(e.value);
+//     document.getElementById('dicebtn').removeAttribute('disabled');
+//     // movePawn('y1', e.value);
+// });
 
 function rollDice() {
-    const number = Math.floor((Math.random() * 6) + 1);
+    // const number = Math.floor((Math.random() * 6) + 1);
+    let number = Math.floor((Math.random() * 10) + 1);
+    number = number > 6 ? 6 : number;
     const current = Number.parseInt(document.getElementById('dice').dataset.number || '1');
     document.getElementById('dice').setAttribute('class', 'dice show-' + String(7-current));
     setTimeout(() => {
         document.getElementById('dice').setAttribute('class', 'dice show-' + String(number));
     }, 200);
     setTimeout(() => {
-        const event = new Event('thrown');
-        event.value = number;
-        dice = number;
-        document.getElementById('dice').dispatchEvent(event);
-        document.getElementById('dice').dataset.number = String(number);
+        // const event = new Event('thrown');
+        // event.value = number;
+        // document.getElementById('dice').dispatchEvent(event);
+        game.dice = number;
+        playGame();
+        // document.getElementById('dice').dataset.number = String(number);
     }, 500);
 }
 
@@ -391,20 +420,21 @@ function clickPawn(pawn) {
 }
 
 // startknop op namen formulier afhandelen
-document.getElementById('startbutton').addEventListener('click', (e) => {
-    e.preventDefault();
-    playernames = [];
-    for (let i = 1; i <= 4; i++) {
-        if (document.getElementById('player' + i.toString()).value) {
-            playernames.push(document.getElementById('player' + i.toString()).value);
+document.getElementById('startbutton').addEventListener('click', () => {
+
+    // verzamel namen
+    let playernames = [];
+    document.querySelectorAll('#names input').forEach((obj) => {
+        if (obj.value.length) {
+            playernames.push(obj.value)
         }
-    }
-    // toon foutmelding als er geen namen zijn ingevuld
+    });
+
+    // toon error
     if (playernames.length < 1) {
         document.getElementById('names-error').classList.remove('hidden');
         return;
     }
-    document.getElementById('names').classList.add('hidden');
     document.getElementById('names-error').classList.add('hidden');
 
     // voeg bots toe
@@ -415,15 +445,179 @@ document.getElementById('startbutton').addEventListener('click', (e) => {
     // hussel spelers
     playernames.sort(() => 0.5 - Math.random());
 
+    // zet spelers op bord
+    playernames.forEach((name, idx) => {
+        players[idx].name = name;
+        document.getElementById('name-' + players[idx].color).textContent = name;
+        players[idx].stats = {
+            turns: 0,
+            total: 0,
+            locked: 0,
+            kicks: 0,
+            kicked: 0
+        };
+    });
+
+    // verberg naamformulier en spelregels
+    document.getElementById('names').classList.add('hidden');
     document.getElementById('rules').classList.add('hidden');
 
-    for (let i = 0; i < 4; i++) {
-        document.getElementById('name' + i).textContent = playernames[i];
-    }
-
+    // toon gameplay element
     document.getElementById('gameplay').classList.remove('hidden');
 
-    document.getElementById('dicebtn').removeAttribute('disabled');
-    console.log(playernames);
+    // start spel
+    player = 0;
+    game.activePlayer = -1;
+    game.state = 'nextPlayer';
+
+    playGame();
 });
 
+function playGame() {
+    switch (game.state) {
+        case 'nextPlayer':
+            game.activePlayer = game.activePlayer < 3 ? game.activePlayer + 1 : 0;
+            // game.activePlayer = 1; //game.activePlayer < 3 ? game.activePlayer + 1 : 0;
+            players[game.activePlayer].stats.turns++;
+            console.log(players[game.activePlayer].name + ' moet gooien');
+            document.getElementById('dicebtn').removeAttribute('disabled');
+            game.state = 'throw';
+            game.throwCount = 0;
+            break;
+        case 'throw':
+            game.throwCount++;
+            console.log(players[game.activePlayer].name + ' heeft ' + game.dice + ' gegooid');
+            // controleer of speler een zet kan doen
+            if (!canMove(players[game.activePlayer].color, game.dice)) {
+                console.log(players[game.activePlayer].name + ' kan niets doen met deze worp');
+                game.state = 'nextPlayer';
+                return playGame();
+            }
+            console.log(players[game.activePlayer].name + ' moet een pion zetten');
+            break;
+        case 'moved':
+            // @todo check for win
+            // volgende speler als geen 6 en niet 2 maal gegooid
+            if ((game.throwCount > 1) || (game.dice !== 6)) {
+                game.state = 'nextPlayer';
+                return playGame();
+            }
+            // nogmaals gooien
+            console.log(players[game.activePlayer].name + ' moet nog een keer gooien');
+            document.getElementById('dicebtn').removeAttribute('disabled');
+            game.state = 'throw';
+            break;
+    }
+}
+
+// controleer of kleur kan zetten
+function canMove(color, dice) {
+    let can = false;
+    for (const [key, pawn] of Object.entries(pawns)) {
+        pawn.o.dataset.movable = '';
+    }
+    for (let i = 1; i <= 4; i++) {
+        const pawn = pawns[color + i.toString()];
+        pawn.n = nextPos(pawn, dice);
+        if (pawn.n) {
+            pawn.o.dataset.movable = 'y';
+            can = true;
+        } else {
+            pawn.o.dataset.movable = 'n';
+        }
+    }
+    return can;
+}
+
+// bepaal positie waar pion naartoe verplaatst bij worp
+function nextPos(pawn, dice) {
+    if (typeof pawn === 'string') {
+        pawn = pawns[pawn];
+    }
+    let color = pawn.o.id[0];
+    let pos;
+
+    // pion naar eindpositie?
+    if (pawn.s + dice > 40) {
+        pos = pawn.s + dice - 40;
+        if (pos > 4) {
+            pos = 4 - (pos - 4);
+        }
+        pos = color + 'f' + pos.toString();
+        // positie vrij?
+        return (positions[pos] !== undefined) && (positions[pos].p === undefined) ? pos : null;
+    }
+
+    // pion naar startpositie of verpaatsen op bord?
+    if (pawn.s === 0) {
+        if (dice < 6) {
+            return null;
+        }
+        pos = start[color];
+    } else {
+        pos = pawn.p + dice;
+        if (pos > 40) {
+            pos -= 40;
+        }
+    }
+    // voorkom zelf-slag
+    console.log(pos);
+    return positions[pos].p !== undefined && positions[pos].p[0] === color ? null : pos;
+}
+
+// verzet pion
+function movePawn(pawn) {
+    const pawnId = pawn.o.id;
+    if (positions[pawn.n].p !== undefined) {
+        returnPawn(positions[pawn.n].p);
+    }
+    console.log('verplaats #' + pawnId + ' naar ' + pawn.n);
+    delete positions[pawn.p].p;
+    positions[pawn.n].p = pawnId;
+    positions[pawn.n].o.classList.remove('highlight');
+    pawn.p = pawn.n;
+    pawn.o.setAttribute('transform', 'translate(' +
+        positions[pawn.p].x + ',' +
+        positions[pawn.p].y + '),rotate(15)');
+    for (const [key, pawn] of Object.entries(pawns)) {
+        delete pawn.n;
+    }
+    if (pawn.s === 0) {
+        pawn.s = 1;
+    } else if (pawn.p <= 40) {
+        pawn.s += game.dice;
+    } else if (pawn.p[1] === 'f') {
+        pawn.s = 40 + Number(pawn.p[2]);
+    }
+    console.log('steps = ' + pawn.s);
+
+    game.state = 'moved';
+    playGame();
+}
+
+// sla pion van bord
+function returnPawn(pawnId) {
+    const color = pawnId[0];
+    const pawn = pawns[pawnId];
+    for (let i = 1; i <= 4; i++) {
+        if (positions[color + 'h' + i.toString()].p === undefined) {
+            console.log('zet #' + pawnId + ' terug in thuishonk');
+            const newpos = color + 'h' + i.toString();
+            delete positions[pawn.p].p;
+            positions[newpos].p = pawnId;
+            pawn.s = 0;
+            pawn.p = newpos;
+            pawn.o.setAttribute('transform', 'translate(' +
+                positions[pawn.p].x + ',' +
+                positions[pawn.p].y + '),rotate(15)');
+            return true;
+        }
+    }
+    return false;
+}
+
+function resetMovable() {
+    for (const [, pawn] of Object.entries(pawns)) {
+        pawn.o.dataset.movable = '';
+    }
+}
